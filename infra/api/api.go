18ed01734f3523b4e78de7dcf4deb7cc0517a655
Joubert RedRat <eu+git@redrat.com.br>
2021-08-18 08:10:41 -0300

    Fix cors and swagger api url

--- infra/api/api.go
+++ infra/api/api.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -17,7 +16,21 @@ func Run() error {
 	controller := NewController()
 	router := gin.Default()
 
-	router.Use(cors.Default())
+	router.Use(func() gin.HandlerFunc {
+		return func(c *gin.Context) {
+			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Credentials", "true")
+			c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+			c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH,OPTIONS,GET,PUT")
+
+			if c.Request.Method == "OPTIONS" {
+				c.AbortWithStatus(204)
+				return
+			}
+
+			c.Next()
+		}
+	}())
 
 	api := router.Group("/api")
 	api.GET("/health", controller.handleHealth)
